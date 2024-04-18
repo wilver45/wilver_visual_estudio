4723e75ae968714033e3349d4894fa2234edfca6
juanfeMendezQ <125328759+juanfeMendezQ@users.noreply.github.com>
2024-04-18 08:54:56 -0500

    hola

---
+++ actividad.py
@@ -0,0 +1,20 @@
+A=2
+B=5
+suma= A+B
+print("la suma es", suma)
+
+
+C=23
+D=15
+resta=C-D
+print("la resta es", resta)
+
+E= 40
+F=59
+multiplicar=E*F
+print("la multiplicacion es", multiplicar)
+
+H=90
+G=70
+division=H/G
+print("la division es", division)
