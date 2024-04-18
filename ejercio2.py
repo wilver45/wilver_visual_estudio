e95f8f22656f6dcdbbb9cd1020a32d780b2884ed
wilver45 <wilvermanjarresv@gmail.com>
2024-04-18 11:35:30 -0500

    commit1

---
+++ ejercio2.py
@@ -0,0 +1,20 @@
+A=int (input ("digite el valor para a: "))
+b=int(input(" digite el valor para b:"))
+suma= A+b
+print ("la suma es: ", suma)
+
+C=int (input ("digite el valor para a: "))
+D=int(input(" digite el valor para b:"))
+resta= C-D
+print ("la suma es: ", resta)
+
+E=int (input ("digite el valor para a: "))
+F=int(input(" digite el valor para b:"))
+multiplicar= E*F
+print ("la suma es: ", multiplicar)
+H=int (input ("digite el valor para h: "))
+G=int(input(" digite el valor para g:"))
+division= H/G
+print ("la suma es: ", division)
+
+
---
+++ ejrecio 3.py
@@ -0,0 +1,4 @@
+compra=int (input ("digite el valor para la comra: "))
+billete=int(input(" digite el valor para bilete:"))
+resta= billete-compra
+print ("la vueltas son : ", resta)
